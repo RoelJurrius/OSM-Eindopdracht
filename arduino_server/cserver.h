d7c634664461a069acbad8e877bbc96d973266b4
RoelJurrius <r.jurrius@student.han.nl>
2026-04-03 11:18:01 +0200

    Arduino server werkt volledig. Zowel de integratie- als de acceptatietests slagen volledig

--- arduino_server/cserver.h
+++ arduino_server/cserver.h
@@ -2,31 +2,22 @@
 #define cserver_h
 
 #include "stream.h"
-#include <stdbool.h> // ik denk dat deze hier overbodig is
 
-
-bool handleRequest(struct stream stream);
-const char* handleResponse(struct stream stream);
-
-/*
-
-// Op moment dat je overgaat van unittests naar integratietests
-// krijgt `handleRequest` een nieuw return-type.
-// Zonder het nu te controleren, verwacht ik dat de onderstaande
-// definitie werkt.
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 enum statuscode {
-  INTERNAL_SERVER_ERROR_500, // failed to malloc cbuffers
-  BAD_REQUEST_400,           // bad request
-  NOT_FOUND_404,             // request target not found
-  OK_200_GET_AVG,            // get mean
-  OK_200_GET_STDEV,          // get standard deviation
-  OK_200_GET_ACTUAL,         // empty cbuffer, get its mean
-  CREATED_201_PUT_MODE_ACTIVE,    // start reading sensors
-  CREATED_201_PUT_MODE_PASSIVE,   // stop reading sensors
-  CREATED_201_PUT_CBUFFSIZE,      // send new cbuffer size
-  CREATED_201_POST_MEASUREMENT,   // send a measurement
-  CREATED_201_DELETE_MEASUREMENTS // clear collected data
+  BAD_REQUEST_400,
+  NOT_FOUND_404,
+  OK_200_GET_AVG,
+  OK_200_GET_STDEV,
+  OK_200_GET_ACTUAL,
+  CREATED_201_PUT_MODE_ACTIVE,
+  CREATED_201_PUT_MODE_PASSIVE,
+  CREATED_201_PUT_CBUFFSIZE,
+  CREATED_201_POST_MEASUREMENT,
+  CREATED_201_DELETE_MEASUREMENTS
 };
 
 struct response {
@@ -38,7 +29,11 @@ struct response {
   };
 };
 
-struct response handleRequest(struct stream);
-*/
+struct response handleRequest(struct stream stream);
+const char* handleResponse(struct stream stream);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
